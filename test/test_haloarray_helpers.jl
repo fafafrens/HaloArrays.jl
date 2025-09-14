8430ab5142724e8020857b033149bfec4923f2ab
Eduardo <grossi.eduard@gmail.com>
2025-09-14 22:09:19 +0200

    tests: qualify normalize_boundary_condition via module to avoid name resolution issues

--- test/test_haloarray_helpers.jl
+++ test/test_haloarray_helpers.jl
@@ -10,30 +10,30 @@ using HaloArrays
 
     @testset "normalize_boundary_condition" begin
         # single symbol -> same BC in all dims
-        bc1 = normalize_boundary_condition(:repeating, 2)
+        bc1 = HaloArrays.normalize_boundary_condition(:repeating, 2)
         @test length(bc1) == 2
         @test bc1[1][1] isa Repeating && bc1[1][2] isa Repeating
         @test bc1[2][1] isa Repeating && bc1[2][2] isa Repeating
 
         # tuple of per-dimension specs (Symbols)
-        bc2 = normalize_boundary_condition((:reflecting, :periodic), 2)
+        bc2 = HaloArrays.normalize_boundary_condition((:reflecting, :periodic), 2)
         @test bc2[1][1] isa Reflecting && bc2[1][2] isa Reflecting
         @test bc2[2][1] isa Periodic && bc2[2][2] isa Periodic
 
         # tuple of per-dimension pairs
-        bc3 = normalize_boundary_condition(((:reflecting, :repeating), (:periodic, :periodic)), 2)
+        bc3 = HaloArrays.normalize_boundary_condition(((:reflecting, :repeating), (:periodic, :periodic)), 2)
         @test bc3[1][1] isa Reflecting && bc3[1][2] isa Repeating
         @test bc3[2][1] isa Periodic && bc3[2][2] isa Periodic
 
         # bad length -> error
-        @test_throws ArgumentError normalize_boundary_condition((:repeating,), 2)
+        @test_throws ArgumentError HaloArrays.normalize_boundary_condition((:repeating,), 2)
     end
 
     @testset "uninitialized HaloArray constructor (undef)" begin
         # costruttore parametric: HaloArray{T,N,A,Halo}(undef, Array, bc)
         # crea HaloArray non-inizializzata per Array{T,N} con halo=1
 
-        bc=normalize_boundary_condition((:repeating, :repeating), 2)
+        bc=HaloArrays.normalize_boundary_condition((:repeating, :repeating), 2)
         h = HaloArray{Float64,2,Array{Float64,2},1}(undef, bc)
 
         @test eltype(h) === Float64
