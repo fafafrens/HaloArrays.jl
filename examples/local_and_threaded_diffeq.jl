ca0f7b047a0445bfc01cbec2c7279f9d4b846fa9
Eduardo Grossi <grossi.eduard@gmail.com>
2026-05-21 12:42:49 +0200

    reuse heat diffusion helpers in examples

--- examples/local_and_threaded_diffeq.jl
+++ examples/local_and_threaded_diffeq.jl
@@ -1,70 +1,10 @@
 using Printf
 using DifferentialEquations
-using HaloArrays
-using OhMyThreads: @tasks
+
+include("heat_diffusion_common.jl")
 
 const ALPHA = 1.0
 const CFL = 0.2
-
-function stable_heat_dt(alpha, cfl, dx)
-    return cfl / (alpha * sum(inv(abs2(d)) for d in dx))
-end
-
-function initial_condition(I, global_dims)
-    center = ntuple(d -> (global_dims[d] + 1) / 2, Val(length(global_dims)))
-    widths = ntuple(d -> global_dims[d] / 10, Val(length(global_dims)))
-    exponent = sum(((I[d] - center[d]) / widths[d])^2 for d in eachindex(I))
-    return 1.0 + exp(-exponent)
-end
-
-function fill_initial_condition!(u)
-    gsize = global_size(u)
-    for I in CartesianIndices(axes(u))
-        u[Tuple(I)...] = initial_condition(Tuple(I), gsize)
-    end
-    synchronize_halo!(u)
-    return u
-end
-
-function heat_rhs!(du::LocalHaloArray, u::LocalHaloArray, p, t)
-    alpha, dx = p
-    synchronize_halo!(u)
-
-    u_data = parent(u)
-    du_data = parent(du)
-    offsets = CartesianIndex.(versors(Val(ndims(u))))
-
-    @inbounds for I in CartesianIndices(interior_range(u))
-        laplacian = zero(eltype(u))
-        for dim in 1:ndims(u)
-            offset = offsets[dim]
-            laplacian += (u_data[I + offset] - 2 * u_data[I] + u_data[I - offset]) / dx[dim]^2
-        end
-        du_data[I] = alpha * laplacian
-    end
-    return du
-end
-
-function heat_rhs!(du::ThreadedHaloArray, u::ThreadedHaloArray, p, t)
-    alpha, dx = p
-    synchronize_halo!(u)
-    offsets = CartesianIndex.(versors(Val(ndims(u))))
-
-    @tasks for tile_id in 1:tile_count(u)
-        u_data = tile_parent(u, tile_id)
-        du_data = tile_parent(du, tile_id)
-
-        @inbounds for I in CartesianIndices(interior_range(u, tile_id))
-            laplacian = zero(eltype(u))
-            for dim in 1:ndims(u)
-                offset = offsets[dim]
-                laplacian += (u_data[I + offset] - 2 * u_data[I] + u_data[I - offset]) / dx[dim]^2
-            end
-            du_data[I] = alpha * laplacian
-        end
-    end
-    return du
-end
 
 function solve_heat_diffeq(u0; alpha=ALPHA, cfl=CFL, domain_length=(1.0, 1.0), nt=100)
     dx = ntuple(d -> domain_length[d] / global_size(u0)[d], Val(ndims(u0)))
@@ -77,24 +17,15 @@ function solve_heat_diffeq(u0; alpha=ALPHA, cfl=CFL, domain_length=(1.0, 1.0), n
     return u, dt
 end
 
-function interior_mean(u::LocalHaloArray)
-    return sum(interior_view(u)) / length(u)
-end
-
-function interior_mean(u::ThreadedHaloArray)
-    total = sum(tile_id -> sum(interior_view(u, tile_id)), 1:tile_count(u))
-    return total / length(u)
-end
-
 function run_local_example()
     u0 = LocalHaloArray(Float64, (64, 64), 1; boundary_condition=:periodic)
-    fill_initial_condition!(u0)
+    fill_centered_gaussian!(u0; baseline=1.0, amplitude=1.0)
     return solve_heat_diffeq(u0)
 end
 
 function run_threaded_example()
     u0 = ThreadedHaloArray(Float64, (32, 32), 1; dims=(2, 2), boundary_condition=:periodic)
-    fill_initial_condition!(u0)
+    fill_centered_gaussian!(u0; baseline=1.0, amplitude=1.0)
     return solve_heat_diffeq(u0)
 end
 
--- examples/local_and_threaded_halo_arrays.jl
+++ examples/local_and_threaded_halo_arrays.jl
@@ -1,101 +1,27 @@
 using Printf
-using HaloArrays
-using OhMyThreads: @tasks
+
+include("heat_diffusion_common.jl")
 
 const ALPHA = 1.0
 const CFL = 0.2
 
-function stable_heat_dt(alpha, cfl, dx)
-    return cfl / (alpha * sum(inv(abs2(d)) for d in dx))
-end
+function run_local_example(; n=(64, 64), nt=100)
+    u = LocalHaloArray(Float64, n, 1; boundary_condition=:periodic)
+    dx = ntuple(d -> 1.0 / n[d], Val(2))
+    dt = stable_heat_dt(ALPHA, CFL, dx)
 
-function initial_condition(I, global_dims)
-    center = ntuple(d -> (global_dims[d] + 1) / 2, Val(length(global_dims)))
-    widths = ntuple(d -> global_dims[d] / 10, Val(length(global_dims)))
-    exponent = sum(((I[d] - center[d]) / widths[d])^2 for d in eachindex(I))
-    return 1.0 + exp(-exponent)
-end
-
-function fill_initial_condition!(u)
-    gsize = global_size(u)
-    for I in CartesianIndices(axes(u))
-        u[Tuple(I)...] = initial_condition(Tuple(I), gsize)
-    end
-    synchronize_halo!(u)
-    return u
-end
-
-function heat_step!(next::LocalHaloArray, current::LocalHaloArray, alpha, dt, dx)
-    old_data = parent(current)
-    next_data = parent(next)
-    offsets = CartesianIndex.(versors(Val(ndims(current))))
-
-    @inbounds for I in CartesianIndices(interior_range(current))
-        laplacian = zero(eltype(current))
-        for dim in 1:ndims(current)
-            offset = offsets[dim]
-            laplacian += (old_data[I + offset] - 2 * old_data[I] + old_data[I - offset]) / dx[dim]^2
-        end
-        next_data[I] = old_data[I] + alpha * dt * laplacian
-    end
-    return next
-end
-
-function heat_step!(next::ThreadedHaloArray, current::ThreadedHaloArray, alpha, dt, dx)
-    offsets = CartesianIndex.(versors(Val(ndims(current))))
-
-    @tasks for tile_id in 1:tile_count(current)
-        old_data = tile_parent(current, tile_id)
-        next_data = tile_parent(next, tile_id)
-
-        @inbounds for I in CartesianIndices(interior_range(current, tile_id))
-            laplacian = zero(eltype(current))
-            for dim in 1:ndims(current)
-                offset = offsets[dim]
-                laplacian += (old_data[I + offset] - 2 * old_data[I] + old_data[I - offset]) / dx[dim]^2
-            end
-            next_data[I] = old_data[I] + alpha * dt * laplacian
-        end
-    end
-    return next
-end
-
-function solve_heat(u0; alpha=ALPHA, cfl=CFL, domain_length=(1.0, 1.0), nt=100)
-    dx = ntuple(d -> domain_length[d] / global_size(u0)[d], Val(ndims(u0)))
-    dt = stable_heat_dt(alpha, cfl, dx)
-    current = copy(u0)
-    next = similar(u0)
-
-    for _ in 1:nt
-        synchronize_halo!(current)
-        heat_step!(next, current, alpha, dt, dx)
-        current, next = next, current
-    end
-
-    synchronize_halo!(current)
-    return current, dt
-end
-
-function interior_mean(u::LocalHaloArray)
-    return sum(interior_view(u)) / length(u)
-end
-
-function interior_mean(u::ThreadedHaloArray)
-    total = sum(tile_id -> sum(interior_view(u, tile_id)), 1:tile_count(u))
-    return total / length(u)
-end
-
-function run_local_example()
-    u0 = LocalHaloArray(Float64, (64, 64), 1; boundary_condition=:periodic)
-    fill_initial_condition!(u0)
-    u, dt = solve_heat(u0)
+    fill_centered_gaussian!(u; baseline=1.0, amplitude=1.0)
+    solve_heat!(u; alpha=ALPHA, dt, dx, nt)
     return u, dt
 end
 
-function run_threaded_example()
-    u0 = ThreadedHaloArray(Float64, (32, 32), 1; dims=(2, 2), boundary_condition=:periodic)
-    fill_initial_condition!(u0)
-    u, dt = solve_heat(u0)
+function run_threaded_example(; tile_size=(32, 32), dims=(2, 2), nt=100)
+    u = ThreadedHaloArray(Float64, tile_size, 1; dims, boundary_condition=:periodic)
+    dx = ntuple(d -> 1.0 / global_size(u)[d], Val(2))
+    dt = stable_heat_dt(ALPHA, CFL, dx)
+
+    fill_centered_gaussian!(u; baseline=1.0, amplitude=1.0)
+    solve_heat!(u; alpha=ALPHA, dt, dx, nt)
     return u, dt
 end
 
